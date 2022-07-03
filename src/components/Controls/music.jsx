c076518eb9899b804eb95cedde3bc90b37b0895d
jerry <1225567135@qq.com>
2022-07-03 23:15:12 +0800

    feay:update

--- src/components/Controls/music.jsx
+++ src/components/Controls/music.jsx
@@ -1,5 +1,5 @@
 
-import { useState} from "react";
+import { useState,useRef} from "react";
 import './music.scss'
 function MusicControls() {
     let [musicData,setMusicData] = useState({
@@ -10,11 +10,38 @@ function MusicControls() {
         songList:[],
         playIndex:0,
     })
+    let progressBtn = useRef()
+    let [drop,setDrop] = useState(false)
+
+    let [dropData,setDropData] = useState({
+        x:'',
+        y:''
+    })
+
     function changeData (){
         setMusicData((c)=>{
             c.songName = '遍地撒安抚'
             return {...c}
         })
+    }
+    function bar_down(e){
+        setDrop(true)
+        setDropData((c)=>{
+            return {x:e.clientX,y:e.clientY}
+        })
+        // progressBtn.current.style.display  = ' none'
+        // console.log(progressBtn)
+        // console.log(this.progressBtn)
+    }
+    function bar_move(e){
+        if(drop){
+            let s_x  = dropData.x -e.clientX
+            console.log(progressBtn.current.style,s_x)
+            // progressBtn.current.style.transform = `translate(${s_x}'px', 0)`
+        }
+    }
+    function bar_up(e){
+        setDrop(false)
     }
     return  (
         <div className="music_con_wrapper">
@@ -24,7 +51,12 @@ function MusicControls() {
                     <p className="song_name">{musicData.songName}</p>
                 </div>
             </div>
-            <div className="controls_progress"></div>
+            <div className="controls_progress">
+                <div className="progress_box" onMouseMove={bar_move} onMouseUp={bar_up}>
+                    <i className="progress_btn" ref={progressBtn}  onMouseDown={bar_down} ></i>
+                    <div className="progress_bar"></div>
+                </div>
+            </div>
         </div>
     )
 }
